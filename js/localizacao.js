b9ff560fe280e1b3d34b4ff250a84027efcf87bb
Hugo Novais <hugo.o.novais@outlook.com>
2021-05-15 15:43:57 -0300

    Localizacao

---
+++ js/localizacao.js
@@ -0,0 +1,9 @@
+function init_map() {
+    var myOptions = { zoom: 13, center: new google.maps.LatLng(-20.348933, -40.405684), mapTypeId: google.maps.MapTypeId.ROADMAP };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+    marker = new google.maps.Marker({ map: map, position: new google.maps.LatLng(-20.348933, -40.405684) });
+    infowindow = new google.maps.InfoWindow({ content: '<strong> MINHA EMPRESA </strong><br> MEU ENDEREÇO <br>' });
+    google.maps.event.addListener(marker, 'click', function() { infowindow.open(map, marker); });
+    infowindow.open(map, marker);
+}
+google.maps.event.addDomListener(window, 'load', init_map);
