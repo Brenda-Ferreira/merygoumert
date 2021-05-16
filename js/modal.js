6cdc2f0f8031b9951970cef92dfcc3fe98877e27
Brenda Ferreira <72113756+Brendafs26@users.noreply.github.com>
2021-05-16 01:39:28 -0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,129 @@
+const modal = document.querySelector(".modal");
+const openModal = document.querySelector("#botaomais");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.onclick = function() {
+    modal.style.display = "flex";
+}
+
+closeModal.onclick = function(){
+    modal.style.display = "none";
+}
+
+modal.onclick = function(event){
+    if(event.target == modal){
+    modal.style.display = "none";
+}
+
+}
+
+
+const modal2 = document.querySelector(".modal2");
+const openModal2 = document.querySelector("#botaomais2");
+const closeModal2 = document.querySelector(".close-modal2");
+
+
+openModal2.onclick = function() {
+    modal2.style.display = "flex";
+}
+
+closeModal2.onclick = function(){
+    modal2.style.display = "none";
+}
+
+
+modal2.onclick = function(event){
+    if(event.target == modal2){
+    modal2.style.display = "none";
+}
+
+
+}
+
+const modal3 = document.querySelector(".modal3");
+const openModal3 = document.querySelector("#botaomais3");
+const closeModal3 = document.querySelector(".close-modal3");
+
+
+openModal3.onclick = function() {
+    modal3.style.display = "flex";
+}
+
+closeModal3.onclick = function(){
+    modal3.style.display = "none";
+}
+
+
+modal3.onclick = function(event){
+    if(event.target == modal3){
+    modal3.style.display = "none";
+}
+
+
+}
+
+const modal4 = document.querySelector(".modal4");
+const openModal4 = document.querySelector("#botaomais4");
+const closeModal4 = document.querySelector(".close-modal4");
+
+
+openModal4.onclick = function() {
+    modal4.style.display = "flex";
+}
+
+closeModal4.onclick = function(){
+    modal4.style.display = "none";
+}
+
+
+modal4.onclick = function(event){
+    if(event.target == modal4){
+    modal4.style.display = "none";
+}
+
+
+}
+
+const modal5 = document.querySelector(".modal5");
+const openModal5 = document.querySelector("#botaomais5");
+const closeModal5 = document.querySelector(".close-modal5");
+
+
+openModal5.onclick = function() {
+    modal5.style.display = "flex";
+}
+
+closeModal5.onclick = function(){
+    modal5.style.display = "none";
+}
+
+
+modal5.onclick = function(event){
+    if(event.target == modal4){
+    modal5.style.display = "none";
+}
+
+
+}
+
+const modal6 = document.querySelector(".modal6");
+const openModal6 = document.querySelector("#botaomais6");
+const closeModal6 = document.querySelector(".close-modal6");
+
+
+openModal6.onclick = function() {
+    modal6.style.display = "flex";
+}
+
+closeModal6.onclick = function(){
+    modal6.style.display = "none";
+}
+
+
+modal6.onclick = function(event){
+    if(event.target == modal4){
+    modal6.style.display = "none";
+}
+
+
+}
