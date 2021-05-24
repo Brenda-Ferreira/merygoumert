08f719d91655b0b213ab1745561664887002d92b
Hugo Novais <hugo.o.novais@outlook.com>
2021-05-24 19:32:08 -0300

    menu atualizado

--- js/cabecalho.js
+++ js/cabecalho.js
@@ -12,6 +12,20 @@ $('#animaMenu').click(function(e) {
 
 });
 
+$('#animaMenu2').click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav').innerHeight();
+
+
+
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+
+});
+
 $('#animaVip').click(function(e) {
     e.preventDefault();
     var id = $(this).attr('href'),
