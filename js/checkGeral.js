127641266b1ba2b4098d79d2d464ed93cb6408e8
Arthur dos Santos Rangel Silva <arthursrsilva@gmail.com>
2021-05-26 12:42:15 -0300

    att checkbox marmitas

--- js/checkGeral.js
+++ js/checkGeral.js
@@ -5,23 +5,24 @@
                                             var CheckMaximo = maximo;
                                             var formSelecionado = form;
                                             var selecaoForm = selecao;
+                                            maximo=0;
 
                                             var Marcados = 1;
                                             var objCheck = document.forms[formSelecionado].elements[selecaoForm];
                                             
-                                            //Percorrendo os checks para ver quantos foram selecionados:
+                                            
                                             for (var iLoop = 0; iLoop<objCheck.length; iLoop++) {
-                                                //Se o número máximo de checkboxes ainda não tiver sido atingido, continua a verificação:
+                                                
                                                 if (Marcados <= CheckMaximo) {
                                                     if (objCheck[iLoop].checked) {
                                                         Marcados++;
                                                     }
-                                                        //Habilitando todos os checkboxes, pois o máximo ainda não foi alcançado.
+                                                        
                                                         for (var i=0; i<objCheck.length; i++) {
                                                             objCheck[i].disabled = false;
                                                         }	
                                                 //Caso contrário, desabilitar o checkbox;
-                                                //Nesse caso, é necessário percorrer todas as opções novamente, desabilitando as não checadas;
+	                                            //Nesse caso, é necessário percorrer todas as opções novamente, desabilitando as não checadas;
                                                 } else {
                                                     for (var i=0; i<objCheck.length; i++) {
                                                         if(objCheck[i].checked == false) {
@@ -29,5 +30,5 @@
                                                         }	
                                                     }
                                                 }
-                                            }
+                                            }                        
                                             };
