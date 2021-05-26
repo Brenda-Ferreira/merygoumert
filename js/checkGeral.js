c585a99948a26362c998aba4bd487e84c6b837f0
Arthur dos Santos Rangel Silva <arthursrsilva@gmail.com>
2021-05-26 00:15:56 -0300

    new js checkbox

---
+++ js/checkGeral.js
@@ -0,0 +1,33 @@
+
+
+
+                                        function checkGeral(maximo, form, selecao) {
+                                            var CheckMaximo = maximo;
+                                            var formSelecionado = form;
+                                            var selecaoForm = selecao;
+
+                                            var Marcados = 1;
+                                            var objCheck = document.forms[formSelecionado].elements[selecaoForm];
+                                            
+                                            //Percorrendo os checks para ver quantos foram selecionados:
+                                            for (var iLoop = 0; iLoop<objCheck.length; iLoop++) {
+                                                //Se o número máximo de checkboxes ainda não tiver sido atingido, continua a verificação:
+                                                if (Marcados <= CheckMaximo) {
+                                                    if (objCheck[iLoop].checked) {
+                                                        Marcados++;
+                                                    }
+                                                        //Habilitando todos os checkboxes, pois o máximo ainda não foi alcançado.
+                                                        for (var i=0; i<objCheck.length; i++) {
+                                                            objCheck[i].disabled = false;
+                                                        }	
+                                                //Caso contrário, desabilitar o checkbox;
+                                                //Nesse caso, é necessário percorrer todas as opções novamente, desabilitando as não checadas;
+                                                } else {
+                                                    for (var i=0; i<objCheck.length; i++) {
+                                                        if(objCheck[i].checked == false) {
+                                                            objCheck[i].disabled = true;
+                                                        }	
+                                                    }
+                                                }
+                                            }
+                                            };
