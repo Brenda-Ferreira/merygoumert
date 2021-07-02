eb4a7a827850902948677bc987db0925987d5ffc
Arthur dos Santos Rangel Silva <arthursrsilva@gmail.com>
2021-07-02 01:28:55 -0300

    fase de teste envio itens carrinho
    a função já recolhe a lista, porém se desmarca a box, ela repete o item.

--- js/checkGeral.js
+++ js/checkGeral.js
@@ -1,34 +1,60 @@
+var listamarmita = ['a'];
+
+function checkGeral(maximo, form, selecao, marmitaitem) {
+    var CheckMaximo = maximo;
+    var formSelecionado = form;
+    var selecaoForm = selecao;
+    var marmitaitem = marmitaitem;
+    maximo = 0;
+
+    var Marcados = 1;
+    var objCheck = document.forms[formSelecionado].elements[selecaoForm];
+    var obj = document.getElementsByName(marmitaitem);
+
+    if (!obj.checked) {
+        //var indice = listamarmita.indexOf(marmitaitem);
+        //listamarmita.splice(indice, 1);
+        listamarmita.unshift(marmitaitem);
+        // listamarmita.unshift("teste");
+    } else {
+        var indice = listamarmita.indexOf(marmitaitem);
+        listamarmita.splice(indice, 1);
+    }
+
+    //listamarmita.unshift(marmitaitem);
+
+    /*  if (objCheck[i].checked == false) {
+        objCheck[i].disabled = true;
+        var indice = listamarmita.indexOf(marmitaitem);
+        listamarmita.splice(indice, 1);
+
+    } else {
+        listamarmita.unshift(marmitaitem);
+    }
+*/
+    for (var iLoop = 0; iLoop < objCheck.length; iLoop++) {
+
+        if (Marcados <= CheckMaximo) {
+            if (objCheck[iLoop].checked) {
+                Marcados++;
+            }
+
+            for (var i = 0; i < objCheck.length; i++) {
+                objCheck[i].disabled = false;
+            }
+            //Caso contrário, desabilitar o checkbox;
+            //Nesse caso, é necessário percorrer todas as opções novamente, desabilitando as não checadas;
+        } else {
+            for (var i = 0; i < objCheck.length; i++) {
+                if (objCheck[i].checked == false) {
+                    objCheck[i].disabled = true;
 
 
+                }
+            }
+        }
 
-                                        function checkGeral(maximo, form, selecao) {
-                                            var CheckMaximo = maximo;
-                                            var formSelecionado = form;
-                                            var selecaoForm = selecao;
-                                            maximo=0;
+    }
+    console.log(listamarmita);
 
-                                            var Marcados = 1;
-                                            var objCheck = document.forms[formSelecionado].elements[selecaoForm];
-                                            
-                                            
-                                            for (var iLoop = 0; iLoop<objCheck.length; iLoop++) {
-                                                
-                                                if (Marcados <= CheckMaximo) {
-                                                    if (objCheck[iLoop].checked) {
-                                                        Marcados++;
-                                                    }
-                                                        
-                                                        for (var i=0; i<objCheck.length; i++) {
-                                                            objCheck[i].disabled = false;
-                                                        }	
-                                                //Caso contrário, desabilitar o checkbox;
-	                                            //Nesse caso, é necessário percorrer todas as opções novamente, desabilitando as não checadas;
-                                                } else {
-                                                    for (var i=0; i<objCheck.length; i++) {
-                                                        if(objCheck[i].checked == false) {
-                                                            objCheck[i].disabled = true;
-                                                        }	
-                                                    }
-                                                }
-                                            }                        
-                                            };
+};
